a303bc2bc36b2459606ba4aa60d09a13c7a23406
Mateus Schiavi <mtsschiavi@gmail.com>
2023-08-16 19:05:10 -0300

    ADDED

---
+++ main.js
@@ -0,0 +1,44 @@
+
+
+/* Los siguientes nombres de funciones son una sugerencia de funciones que necesitarás en tu programa,
+sin embargo, no te limites solo a estas funciones. Crea tantas como consideres necesarias.
+
+La estructura de cada objeto "tarea" es la siguiente:
+
+{
+  id: 1,
+  title: "tarea",
+  completed: false
+}
+
+*/
+
+// Función para añadir una nueva tarea
+function addTask() {
+
+}
+
+// Función para marcar una tarea como completada o imcompleta (Puede ser la misma función)
+function completeTask() {
+
+}
+
+// Función para borrar una tarea
+function deleteTask() {
+
+}
+
+// Funcion para borrar todas las tareas
+function deleteAll() {
+
+}
+
+// Función para filtrar tareas completadas
+function filterCompleted() {
+
+}
+
+// Función para filtrar tareas incompletas
+function filterUncompleted() {
+
+}
